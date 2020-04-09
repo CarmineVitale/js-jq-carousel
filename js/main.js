45a2b1a055255a2eb1301c9cece2bc3b246c2dc5
CarmineVitale <carmine.vitale90@gmail.com>
2020-04-09 19:18:37 +0200

    Soluzione JS non funzionante :(

--- js/main.js
+++ js/main.js
@@ -1,4 +1,50 @@
 $(document).ready(function () {
-    console.log('SLIDER IMMAGINI');
+
+    var indietro = $('.sinistra');
+    var avanti = $('.destra');
+
+    avanti.click(function () { 
+        var imageSelected = $('.active');
+        var nextImage = imageSelected.next();
+    
+        if (imageSelected.hasClass('active')) {
+            imageSelected.removeClass('active');
+            nextImage.addClass('active');
+        }
     
 });
+        indietro.click(function () { 
+            var imageSelected = $('.active');
+             var prevImage = imageSelected.prev();
+
+        if (imageSelected.hasClass('active')) {
+        imageSelected.removeClass('active');
+        prevImage.addClass('active');
+        }
+
+});
+        
+    });
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+//END READY    
+
