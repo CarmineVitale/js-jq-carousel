f5bfd4ce9ad0f82706b51a035d1c7538bf27e9b2
CarmineVitale <carmine.vitale90@gmail.com>
2020-04-09 22:08:22 +0200

    Scorrimento mediante tastiera funzionante

--- js/main.js
+++ js/main.js
@@ -4,35 +4,49 @@ $(document).ready(function () {
     var avanti = $('.destra');
     
        
-
-    avanti.click(function () { 
+//RICHIAMO FUNZIONE
+    avanti.click(next); 
+    indietro.click(back); 
+    //AVANTI E INDIETRO CON TASTIERA
+    $(document).keydown(function (e) { 
+        if (e.keyCode == 37) {
+            back()
+        } else if (e.keyCode == 39) {
+            next()
+        }
+    });
+    $(document).keydown(function (e) {
+        console.log(e.keyCode);
         
+    })
+
+//CREAZIONE FUNZIONE 
+function next() {
         var imageSelected = $('.active');
         var nextImage = imageSelected.next();
         imageSelected.removeClass('active');
-    
         if (imageSelected.hasClass('last')) {
              $('.first').addClass('active');
         } else {
             nextImage.addClass('active')
         }
-    
-});
-        indietro.click(function () { 
-             var imageSelected = $('.active');
-             var prevImage = imageSelected.prev();
-             imageSelected.removeClass('active')
+  }
 
-        if (imageSelected.hasClass('first')) {
-            $('.last').addClass('active');
-        } else {
-            prevImage.addClass('active')
-        }
+function back() {
+    var imageSelected = $('.active');
+    var prevImage = imageSelected.prev();
+    imageSelected.removeClass('active')
 
-});
+    if (imageSelected.hasClass('first')) {
+        $('.last').addClass('active');
+    } else {
+        prevImage.addClass('active')
+    }
+}
         
     });
     
+
     
 
 
